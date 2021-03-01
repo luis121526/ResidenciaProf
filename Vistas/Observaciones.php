aeef3aca4f5f61fae1dba5616c5ce5d800b83a02
luis121526 <diazdiaz121526@gmail.com>
2021-03-01 01:44:07 -0600

    cambios

--- Vistas/Observaciones.php
+++ Vistas/Observaciones.php
@@ -5,6 +5,7 @@
     </head>
     <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="../css/miestilo.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jszip-2.5.0/pdfmake-0.1.18/dt-1.10.12/af-2.1.2/b-1.2.2/b-colvis-1.2.2/b-flash-1.2.2/b-html5-1.2.2/b-print-1.2.2/cr-1.3.2/fc-3.2.2/fh-3.1.2/kt-2.1.3/r-2.1.0/rr-1.1.2/sc-1.4.2/se-1.2.0/datatables.min.css"/>
     <meta charset="UTF-8">
     <body>
          <!-- logotipo-->
@@ -34,8 +35,8 @@
         <!--datos observaciones-->
         <div class="crud">
             <form>
-            <table class="table">
-                <h6 class="text-center"> Eliminar Observaciones </h6>
+            <table class="table" id="obser">
+                <h6 class="text-center"> Opservaciones Permanentes </h6>
                     <thead>
                         <tr>
                         
@@ -74,6 +75,7 @@
         </div>
      <!--datos observaciones-->
      <!--modificar--> 
+     <!--
      <div class="modificar">
          <h6 class="text-center"> Modificar Observaciones </h6>
                         
@@ -81,12 +83,12 @@
                 <div class="form-group">
                     <label for="seleccion">Selecciona ID de Observacion</label>
                    <select>
-                        <?php
+                        <?php/*
                         $query3=mysqli_query($conexion,"SELECT *FROM observaciones");
                         while($fr3=mysqli_fetch_row($query3)){
                             echo "
                             <option>$fr3[0]</option>";
-                        }
+                        }*/
                         ?>
                     </select>
                 </div>
@@ -104,7 +106,16 @@
                 </div>
                 <input type="submit" class="btn btn-warning" value="Modificar"/>
                 </form>
-    </div>
+    </div>-->
+    <!-- jQuery -->
+	<script language="javascript" src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
+	<!-- El JavaScript de DataTables -->
+	<script language="javascript" type="text/javascript" src="https://cdn.datatables.net/v/dt/jszip-2.5.0/pdfmake-0.1.18/dt-1.10.12/af-2.1.2/b-1.2.2/b-colvis-1.2.2/b-flash-1.2.2/b-html5-1.2.2/b-print-1.2.2/cr-1.3.2/fc-3.2.2/fh-3.1.2/kt-2.1.3/r-2.1.0/rr-1.1.2/sc-1.4.2/se-1.2.0/datatables.min.js"></script>
+
+	<!-- El código JavaScript -->
+	<script language="javascript">
+		$('#obser').DataTable();
+	</script>
     </body>
     <footer>
 
